f7cbb21b0e256d48b0111329da81f7612c23ef6c
pkwork-cpu <kermani.pedram6@gmail.com>
2025-03-26 21:35:14 -0500

    CH2L3 v1

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import pygame
 from constants import *
 
 
-
-
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -16,15 +13,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     running = True
+    clock_object = pygame.time.Clock()
+    dt = 0
     while running:
         for event in pygame.event.get():
+            # check for quit event
             if event.type == pygame.QUIT:
                 running = False
-                print("Exiting Asteroids2!")
 
         screen.fill((0, 0, 0))  # fill screen with black
         pygame.display.flip()   # update the display
+        dt = clock_object.tick(60) # limit the frame rate to 60 FPS pause the game loop until 1/60th of a second has passed
+        dt = dt / 1000 # convert from milliseconds to seconds
+        print (dt)
     print("Exiting Asteroids!")
     pygame.quit()
+
+
+
 if __name__ == "__main__":
     main()
